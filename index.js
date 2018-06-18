2a07022ed15db07d558e00ca979252a1275578c9
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-06-18 09:25:34 +0200

    Simplify some code

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 const { string: { singularize, pluralize } } = require('pure-utilities')
 
 module.exports = function negate(string) {
-  const SIMPLE_CONDITIONS = ['gt', 'lt', 'gte', 'lte']
-  if (SIMPLE_CONDITIONS.includes(string)) {
-    return 'n' + string
+  if (['gt', 'lt', 'gte', 'lte'].includes(string)) return `n${string}`
+
+  const firstWord = string.split(' ')[0]
+  if (firstWord === 'is' || firstWord === 'are') {
+    return `${string.substr(0, firstWord.length)} not ${string.substr(firstWord.length + 1)}`
   }
-  if (string.startsWith('is')) {
-    return string.substr(0, 2) + ' not ' + string.substr(3)
+  if (firstWord === 'has' || firstWord === 'have') {
+    return firstWord[firstWord.length - 1] === 's' ? `does not ${string.replace('has', 'have')}` : `do not ${string}`
   }
-  if (string.startsWith('are')) {
-    return string.substr(0, 3) + ' not ' + string.substr(4)
-  }
-  if (string.startsWith('has')) {
-    return 'does not ' + string.replace('has', 'have')
-  }
-  if (string.startsWith('have')) {
-    return 'do not ' + string
-  }
-  if (string.startsWith('bitwise')) {
-    return 'not ' + string
+  if (firstWord === 'bitwise') {
+    return `not ${string}`
   }
   if (string.endsWith('with') || string.endsWith('to')) {
     string = string.split(' ')
     string[0] = string[0].slice(0, string[0].length - 1)
     return 'does not ' + string.join(' ')
   }
-  if (string === 'equals' || string === 'contains') {
-    return 'does not ' + string.substring(0, string.length - 1)
+
+  let singular = singularize(firstWord)
+  let pluaral = pluralize(singular)
+
+  if (pluaral !== firstWord) {
+    singular += 'e'
+    return string
   }
-  if (string === 'matches') {
-    return 'does not match'
-  }
-  if (string === 'includes') {
-    return 'does not include'
-  }
-  return string
+
+  return `does not ${singular}`
 }
